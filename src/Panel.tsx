7a5efe3ea82066adbebe189929259195578c6682
kboul <kos.voulgaridis@gmail.com>
2020-02-22 00:08:53 +0200

    Make Cards expandable by inserting a tooglable icon to expand/collapse them on Panel

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,20 +1,34 @@
-import React, { FC } from 'react';
-import { Card, CardHeader, CardBody, CardText } from 'reactstrap';
+import React, { FC, useState } from 'react';
+import { Card, CardHeader, CardBody, CardText, Collapse } from 'reactstrap';
 
 interface PanelProps {
     header: string;
     text: string;
 }
 
+const cardHeaderStyle = { cursor: 'pointer' };
+
 const Panel: FC<PanelProps> = ({ header, text }) => {
+    const [isCardOpen, setIsOpen] = useState(true);
+    const toggleCard = () => setIsOpen(!isCardOpen);
+
+    const cardIcon = isCardOpen ? 'down' : 'right';
+
     return (
-        <Card outline color="primary" className="mb-4">
-            <CardHeader className="card bg-primary text-white">
-                {header}
+        <Card outline color="primary" className="mb-4" onClick={toggleCard}>
+            <CardHeader
+                className="card bg-primary text-white"
+                style={cardHeaderStyle}>
+                <span>
+                    {header}
+                    <i className={`fa fa-chevron-${cardIcon} float-right`} />
+                </span>
             </CardHeader>
-            <CardBody>
-                <CardText>{text}</CardText>
-            </CardBody>
+            <Collapse isOpen={isCardOpen}>
+                <CardBody>
+                    <CardText>{text}</CardText>
+                </CardBody>
+            </Collapse>
         </Card>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
