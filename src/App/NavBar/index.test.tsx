b2304180d1b8a0547f30a2a4d1a2a537d8c1e15c
kboul <kos.voulgaridis@gmail.com>
2021-04-25 21:38:21 +0300

    Unit test Navbar

---
+++ src/App/NavBar/index.test.tsx
@@ -0,0 +1,39 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { renderWithContext, screen } from '../../tests/utils';
+
+import Navbar from '.';
+
+beforeEach(() => {
+    renderWithContext(<Navbar />, { wrapper: MemoryRouter });
+});
+
+test('app logo appears on navbar ', () => {
+    const appLogo = screen.getByText('WFS Spy');
+    expect(appLogo).toBeInTheDocument();
+});
+
+test('getCapabilities list appears on navbar', () => {
+    const getCapabilitiesList = screen.getByText('GetCapabilities');
+    expect(getCapabilitiesList).toBeInTheDocument();
+});
+
+test('describeFeatureType list appears on navbar', () => {
+    const describeFeatureTypeList = screen.getByText('DescribeFeatureType');
+    expect(describeFeatureTypeList).toBeInTheDocument();
+});
+
+test('statistics link appears on navbar', () => {
+    const statisticsLink = screen.getByText('Statistics');
+    expect(statisticsLink).toBeInTheDocument();
+});
+
+test('reset link appears on navbar', () => {
+    const resetLink = screen.getByText('Reset');
+    expect(resetLink).toBeInTheDocument();
+});
+
+test('navbar has two images', () => {
+    const img = screen.getAllByRole('img');
+    expect(img).toHaveLength(2);
+});
